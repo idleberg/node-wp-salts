33d6e68cac642a14853a86c66fa5aa498ce8c07e
idleberg <git@idleberg.com>
2019-04-09 20:50:58 +0200

    update test

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ import { wpSalts } from '../dist';
 // Dependencies
 import test from 'ava';
 
-const defaultFields = [
+const wpKeys = [
   'AUTH_KEY',
   'AUTH_SALT',
   'LOGGED_IN_KEY',
@@ -16,14 +16,43 @@ const defaultFields = [
 ];
 
 // Tests
-test('Default usage', async t => {
+test('Default: WordPress keys', async t => {
   const actual = Object.keys(wpSalts()).sort();
-  const expected = defaultFields;
+  const expected = wpKeys;
 
   t.deepEqual(expected, actual);
 });
 
-test('Custom key', async t => {
+test('Default: Key length (8-bit)', async t => {
+  const salts = wpSalts();
+  let actual = 0;
+
+  Object.keys(salts).sort().forEach( (key, index) => {
+    actual += salts[key].length;
+  });
+
+  const expected = wpKeys.length * 64;
+
+  t.is(expected, actual);
+});
+
+
+test('Default: Key length (16-bit)', async t => {
+  const keyLength = 128;
+  const salts = wpSalts({}, keyLength);
+
+  let actual = 0;
+
+  Object.keys(salts).sort().forEach( (key, index) => {
+    actual += salts[key].length;
+  });
+
+  const expected = wpKeys.length * keyLength;
+
+  t.is(expected, actual);
+});
+
+test('Custom String: WordPress keys', async t => {
   const key = 'test';
 
   const actual = Object.keys(wpSalts(key));
@@ -32,11 +61,71 @@ test('Custom key', async t => {
   t.deepEqual(expected, actual);
 });
 
-test('Custom keys', async t => {
+test('Custom String: Key length (8-bit)', async t => {
+  const defaultKey = 'test';
+  const salts = wpSalts(defaultKey);
+
+  const actual = salts[defaultKey].length;
+  const expected = 64;
+
+  t.is(expected, actual);
+});
+
+test('Custom String: Key length (16-bit)', async t => {
+  const defaultKey = 'test';
+  const keyLength = 128;
+  const salts = wpSalts(defaultKey, keyLength);
+
+  const actual = salts[defaultKey].length;
+  const expected = keyLength;
+
+  t.is(expected, actual);
+});
+
+test('Custom Array: WordPress keys', async t => {
   const keys = ['test1', 'test2', 'test3']
 
   const actual = Object.keys(wpSalts(keys));
   const expected = keys;
 
   t.deepEqual(expected, actual);
+});
+
+test('Custom Array: Key length (8-bit)', async t => {
+  const defaultKeys = [
+    'test1',
+    'test2',
+    'test3'
+  ];
+  const salts = wpSalts(defaultKeys);
+
+  let actual = 0;
+
+  defaultKeys.forEach( defaultKey => {
+    actual += salts[defaultKey].length
+  });
+
+  const expected = defaultKeys.length * 64;
+
+  t.is(expected, actual);
+});
+
+test('Custom Array: Key length (16-bit)', async t => {
+  const defaultKeys = [
+    'test1',
+    'test2',
+    'test3'
+  ];
+  const keyLength = 128;
+  const salts = wpSalts(defaultKeys, keyLength);
+
+  let actual = 0;
+
+  defaultKeys.forEach( defaultKey => {
+    actual += salts[defaultKey].length
+  });
+
+  const expected = defaultKeys.length * keyLength;
+
+  t.is(expected, actual);
 });
