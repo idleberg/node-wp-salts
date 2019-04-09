195bfbf428b1851d2e2e7c1598cd1a11f52daa56
floatwork_jan <jan@floatwork.at>
2019-04-09 17:12:34 +0200

    add test

--- test/index.js
+++ test/index.js
@@ -6,19 +6,37 @@ import test from 'ava';
 
 const defaultFields = [
   'AUTH_KEY',
-  'SECURE_AUTH_KEY',
-  'LOGGED_IN_KEY',
-  'NONCE_KEY',
   'AUTH_SALT',
-  'SECURE_AUTH_SALT',
+  'LOGGED_IN_KEY',
   'LOGGED_IN_SALT',
-  'NONCE_SALT'
+  'NONCE_KEY',
+  'NONCE_SALT',
+  'SECURE_AUTH_KEY',
+  'SECURE_AUTH_SALT'
 ];
 
 // Tests
 test('Default usage', async t => {
-  const actual = Object.keys(wpSalts());
+  const actual = Object.keys(wpSalts()).sort();
   const expected = defaultFields;
+
+  t.deepEqual(expected, actual);
+});
+
+test('Custom key', async t => {
+  const key = 'test';
+
+  const actual = Object.keys(wpSalts(key));
+  const expected = [ key ];
+
+  t.deepEqual(expected, actual);
+});
+
+test('Custom keys', async t => {
+  const keys = ['test1', 'test2', 'test3']
+
+  const actual = Object.keys(wpSalts(keys));
+  const expected = keys;
 
   t.deepEqual(expected, actual);
 });
