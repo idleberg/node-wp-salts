2b8645b09534917e897301be1f1b3367329adaf2
idleberg <git@idleberg.com>
2020-09-05 20:23:48 +0200

    add rollup-plugin-filesize

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
 import typescript from '@rollup/plugin-typescript';
 
 const defaults = {
@@ -10,6 +11,7 @@ const defaults = {
   },
   plugins: [
     commonjs(),
+    filesize(),
     nodeResolve(),
     typescript()
   ]
