984ba5955d1e71c1c7424c41fe1ede775acc6f0e
Jan T. Sott <git@idleberg.com>
2024-05-30 23:07:56 +0200

    update dependencies, tooling

--- src/util.ts
+++ src/util.ts
@@ -11,31 +11,31 @@ const MINIMUM_KEY_LENGTH = 64;
  * @see https://github.com/EFForg/OpenWireless/blob/0e0bd06277f7178f840c36a9b799c7659870fa57/app/js/diceware.js#L59
  */
 function getRandom(min: number, max: number): number {
-  let randomValue = 0;
-  const range: number = max - min;
+	let randomValue = 0;
+	const range: number = max - min;
 
-  const bitsNeeded: number = Math.ceil(Math.log2(range));
+	const bitsNeeded: number = Math.ceil(Math.log2(range));
 
-  if (bitsNeeded > 53) {
-    throw new RangeError('Cannot generate numbers larger than 53 bits.');
-  }
+	if (bitsNeeded > 53) {
+		throw new RangeError('Cannot generate numbers larger than 53 bits.');
+	}
 
-  const bytesNeeded: number = Math.ceil(bitsNeeded / 8);
-  const mask: number = Math.pow(2, bitsNeeded) - 1;
-  const byteArray: Uint8Array = new Uint8Array(bytesNeeded);
+	const bytesNeeded: number = Math.ceil(bitsNeeded / 8);
+	const mask: number = Math.pow(2, bitsNeeded) - 1;
+	const byteArray: Uint8Array = new Uint8Array(bytesNeeded);
 
-  getRandomValues(byteArray);
+	getRandomValues(byteArray);
 
-  let p = (bytesNeeded - 1) * 8;
+	let p = (bytesNeeded - 1) * 8;
 
-  for (let i = 0; i < bytesNeeded; i++) {
-    randomValue += byteArray[i] * Math.pow(2, p);
-    p -= 8;
-  }
+	for (let i = 0; i < bytesNeeded; i++) {
+		randomValue += byteArray[i] * Math.pow(2, p);
+		p -= 8;
+	}
 
-  randomValue = randomValue & mask;
+	randomValue = randomValue & mask;
 
-  return randomValue >= range ? getRandom(min, max) : min + randomValue;
+	return randomValue >= range ? getRandom(min, max) : min + randomValue;
 }
 
 /**
@@ -45,19 +45,19 @@ function getRandom(min: number, max: number): number {
  * @see https://roots.io/salts.html
  */
 function getRandomChar(): string {
-  const minCharacter = 33;
-  const maxCharacter = 126;
-  const character: string = String.fromCharCode(getRandom(minCharacter, maxCharacter));
+	const minCharacter = 33;
+	const maxCharacter = 126;
+	const character: string = String.fromCharCode(getRandom(minCharacter, maxCharacter));
 
-  if (
-    ['\'', '"', '\\'].some(badCharacter => {
-      return character === badCharacter;
-    })
-  ) {
-    return getRandomChar();
-  }
+	if (
+		["'", '"', '\\'].some((badCharacter) => {
+			return character === badCharacter;
+		})
+	) {
+		return getRandomChar();
+	}
 
-  return character;
+	return character;
 }
 
 /**
@@ -68,11 +68,7 @@ function getRandomChar(): string {
  * @see https://roots.io/salts.html
  */
 function generateSalt(saltLength = MINIMUM_KEY_LENGTH): string {
-  return Array(...Array(saltLength))
-    .map(getRandomChar)
-    .join('');
+	return new Array(...new Array(saltLength)).map(getRandomChar).join('');
 }
 
-export {
-  generateSalt
-};
+export { generateSalt };
